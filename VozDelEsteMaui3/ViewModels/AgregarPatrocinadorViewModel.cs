813ca400b07843499584bc7ed974f8099394e133
Rodrigo <modrips4@gmail.com>
2025-08-20 20:37:29 -0300

    avance

---
+++ VozDelEsteMaui3/ViewModels/AgregarPatrocinadorViewModel.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VozDelEsteMaui3.Modelos;
+
+namespace VozDelEsteMaui3.ViewModels
+{
+   public class AgregarPatrocinadorViewModel : ObservableObject
+   {
+      public Coordenadas UbicacionSeleccionada { get; set; }
+
+      public ICommand ValidarUbicacionCommand { get; }
+
+
+      public AgregarPatrocinadorViewModel()
+      {
+
+      }
+   }
+}
+
---
+++ VozDelEsteMaui3/Vistas/AgregarPatrocinador.xaml.cs
@@ -0,0 +1,37 @@
+using Microsoft.Maui.Controls.Maps;
+using VozDelEsteMaui3.ViewModels;
+
+namespace VozDelEsteMaui3.Vistas;
+
+public partial class AgregarPatrocinador : ContentPage
+{
+   public AgregarPatrocinador(AgregarPatrocinadorViewModel agregarPatrocinadorViewModel)
+   {
+      InitializeComponent();
+
+      BindingContext = agregarPatrocinadorViewModel;
+   }
+
+   private void OnMapClicked(object sender, MapClickedEventArgs e)
+   {
+      var lat = e.Location.Latitude;
+      var lon = e.Location.Longitude;
+
+      // Mostrar marcador
+      Mapa.Pins.Clear();
+      Mapa.Pins.Add(new Pin
+      {
+         Label = "Ubicación seleccionada",
+         Location = e.Location,
+         Type = PinType.Place
+      });
+
+      // Delegar al ViewModel
+      if (BindingContext is AgregarPatrocinadorViewModel vm)
+      {
+         vm.UbicacionSeleccionada = new Coordenadas(lat, lon);
+         vm.ValidarUbicacionCommand?.Execute(null);
+      }
+   }
+
+}
